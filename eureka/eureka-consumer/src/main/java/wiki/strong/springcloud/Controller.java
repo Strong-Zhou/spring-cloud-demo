4042f9316da4374695431b225c84b644577f3c37
strong <314150624@qq.com>
2021-03-13 15:11:05 +0800

    first commit

--- eureka/eureka-consumer/src/main/java/wiki/strong/springcloud/Controller.java
+++ eureka/eureka-consumer/src/main/java/wiki/strong/springcloud/Controller.java
@@ -1,6 +1,5 @@
 package wiki.strong.springcloud;
 
-import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
